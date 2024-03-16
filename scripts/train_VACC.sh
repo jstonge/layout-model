79369c95e20d46e1a597f6db8ee37eda7395b332
jstonge <jonathanstonge7@gmail.com>
2024-03-16 16:53:56 -0400

    small fix

--- scripts/train_VACC.sh
+++ scripts/train_VACC.sh
@@ -3,15 +3,17 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=4
 #SBATCH --gpus=1
-#SBATCH --mem=30G
+#SBATCH --mem=16G
 #SBATCH --time=12:00:00
-
+source ~/myconda.sh
+conda activate catDB
+module load gcc-9.3.0-gcc-7.3.0-fjzqkyt
 python train_net.py \
     --dataset_name          cc \
     --json_annotation_train ../data/train.json \
-    --image_path_train      ../data/images \
+    --image_path_train      ../data/ \
     --json_annotation_val   ../data/test.json \
-    --image_path_val        ../data/images \
+    --image_path_val        ../data/ \
     --config-file           ../configs/fast_rcnn_R_50_FPN_3x.yaml \
     OUTPUT_DIR  ../outputs/ \
     SOLVER.IMS_PER_BATCH 2 
